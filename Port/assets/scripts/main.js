9956c72ac4947b9948a5118f354970b644444404
Vitim <143446454+Vitorcostalv@users.noreply.github.com>
2025-04-22 15:56:52 -0300

    Delete Port directory

--- Port/assets/scripts/main.js
+++
@@ -1,8 +0,0 @@
-// Efeito de digitação no texto
-new Typed("#typed-text", {
-    strings: ["Desenvolvedor Full Stack", "Amante de tecnologia", "Criador de soluções"],
-    typeSpeed: 60,
-    backSpeed: 30,
-    loop: true
-  });
-  
